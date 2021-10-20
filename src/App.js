8519d5623bac298988811b869b39448bbf8106be
Fatima Aminu <teemamin@yahoo.com>
2021-10-20 08:01:19 +0000

    react-bootstrap added to project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
      <h1>Hello world</h1>
+     <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
     </div>
   );
 }
