375d221557dc4820745d538c0e4d43c596d2b570
Fatima Aminu <teemamin@yahoo.com>
2021-10-24 07:45:05 +0000

    basic navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
-import Button from 'react-bootstrap/Button';
+import styles from "./App.module.css";
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello world</h1>
-     <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+    <div className={styles.App}>
+      <NavBar/>
     </div>
   );
 }
