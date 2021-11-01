8ac5f1e68690b0ec26a13c8bcc8e6afe3ca28e42
Fatima Aminu <teemamin@yahoo.com>
2021-11-01 04:57:49 +0000

    siginForm and route added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect, createContext } from "react";
 import styles from "./App.module.css";
 import NavBar from './components/NavBar';
 import Container from "react-bootstrap/Container";
@@ -8,15 +9,33 @@ import {
   Link
 } from "react-router-dom";
 import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
+import axios from "axios";
+
+export const CurrentUserContext = createContext();
 
 function App() {
+  const [currentUser, setCurrentUser]=useState(null)
+
+  const handleMount = async ()=>{
+    try{
+      const {data} = await axios.get("/dj-rest-auth/user/")
+      setCurrentUser(data)
+    }catch(e){
+      console.log(e)
+    }
+  }
+  useEffect(() => {
+    handleMount()
+    
+  }, [])
   return (
     <div className={styles.App}>
       <NavBar/>
       <Container className={styles.Main}>
         <Switch>
           <Route exact path="/" render={()=><h2>Home page </h2>}/>
-          <Route exact path="/signin" render={()=><h2>Sigin</h2>}/>
+          <Route exact path="/signin" render={()=><SignInForm/>}/>
           <Route exact path="/signup" render={()=><SignUpForm/>}/>
           <Route render={() => <p>Page not found!</p>} />
 
